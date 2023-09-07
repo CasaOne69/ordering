abb5241dffb1db2490f33eb0efc1af2377e8eefc
CasaOne69 <oficina@casalock.es>
2023-09-07 18:17:03 +0200

    4

--- index.js
+++ index.js
@@ -1,2 +1,41 @@
 import menuArray from '/data.js'
-console.log(menuArray)
+
+
+function shitfood (dogshit) {
+return dogshit.map(function (ratshit){
+    const {name, ingredients, id, price, emoji} = ratshit
+    return `
+    <section class="fooditemslist">
+        <div class="container">
+            <div><p class="image">${emoji}</p></div>
+                    <div>
+                        <div><h2>${name}</h2></div>
+                        <div> <p>Ingredients: ${ingredients}</p></div>
+                        <div><h3>Price: £${ratshit.price}</h3></div>
+                    </div>
+        </div>
+        <div class="add"></div>
+    </section>
+    <hr>
+    ` 
+}).join("")
+      
+       
+        
+ 
+    
+
+
+
+}
+document.getElementById('items').innerHTML = shitfood(menuArray)
+
+
+
+
+
+    
+
+
+
+
